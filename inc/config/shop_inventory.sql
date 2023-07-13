05cbb78e240652dcbcca7d4f0695c76e79ed08e2
TheUnknownSky <vonnmamaril24@gmail.com>
2023-07-13 16:16:37 +0800

    Updated shop_inventory.sql

--- inc/config/shop_inventory.sql
+++ inc/config/shop_inventory.sql
@@ -1,13 +1,14 @@
 -- phpMyAdmin SQL Dump
--- version 4.5.1
--- http://www.phpmyadmin.net
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Mar 18, 2019 at 03:51 AM
--- Server version: 10.1.13-MariaDB
--- PHP Version: 7.0.5
+-- Generation Time: Jul 13, 2023 at 10:10 AM
+-- Server version: 10.4.28-MariaDB
+-- PHP Version: 8.2.4
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
 SET time_zone = "+00:00";
 
 
@@ -30,32 +31,36 @@ CREATE TABLE `customer` (
   `customerID` int(11) NOT NULL,
   `fullName` varchar(100) NOT NULL,
   `email` varchar(100) DEFAULT NULL,
-  `mobile` int(11) NOT NULL,
-  `phone2` int(11) DEFAULT NULL,
+  `mobile` varchar(10) NOT NULL,
+  `phone2` varchar(10) DEFAULT NULL,
   `address` varchar(255) NOT NULL,
   `address2` varchar(255) DEFAULT NULL,
   `city` varchar(30) DEFAULT NULL,
   `district` varchar(30) NOT NULL,
-  `status` varchar(255) NOT NULL DEFAULT 'Active',
-  `createdOn` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+  `status` varchar(10) NOT NULL DEFAULT 'Active',
+  `createdOn` timestamp NOT NULL DEFAULT current_timestamp(),
+  `password` varchar(72) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
 
 --
 -- Dumping data for table `customer`
 --
 
-INSERT INTO `customer` (`customerID`, `fullName`, `email`, `mobile`, `phone2`, `address`, `address2`, `city`, `district`, `status`, `createdOn`) VALUES
-(4, 'Bill Gates', 'bill@microsoft.com', 993737, 772484884, '45, Palo Alto House, Marine Drive', 'South Carolina', 'Microsoft', 'Kurunegala', 'Active', '2018-04-30 15:14:02'),
-(14, 'Steve Jobs', 'sjobs@apple.com', 333829832, 0, '1st Floor, Apple House, ', 'Las Vegas Street', 'Las Vegas', 'Monaragala', 'Disabled', '2018-05-01 12:03:10'),
-(18, 'Asitha Silva', 'asitha@gmail.com', 777987654, 0, 'No. 3, Radcliff Avenue, School Lane', 'Kalutara South', 'Kalutara', 'Kalutara', 'Active', '2018-05-02 09:52:28'),
-(24, 'Sunil Perera', 'Sunil@gypsies.sound', 338393932, 413837293, '67/7, Perera Villa, Jayasekara Avenue', 'Mount Lavinia', 'Ratmalana', 'Colombo', 'Active', '2018-05-02 10:48:37'),
-(25, 'Theresa May', 'may34@uk.gov.com', 329393903, 777833737, '12, Downing Street', 'London', 'London', 'Matale', 'Active', '2018-05-03 02:28:07'),
-(26, 'Sachin Tendulkar', 'sachintendulkar@icc.com', 444958303, 84792838, '789-4, Apartment 3, ', 'Cric Complex', 'New Delhi', 'Puttalam', 'Active', '2018-05-03 02:28:38'),
-(38, 'Nuwan Perara', 'nuwan@yahoo.com', 839378202, 0, 'Nuwan Villa, Lower Street,', 'North Mulativu', 'Mullaitivu', 'Mullaitivu', 'Active', '2018-05-05 11:17:49'),
-(39, 'Amal Silverton', 'amals452@yahoo.com', 232345676, 0, 'Amal''s House, Amal''s Street,', 'Amal Road', 'Ambalangoda', 'Galle', 'Active', '2018-05-05 13:27:06'),
-(40, 'Andrew Symonds', 'symonds@cricket.au.com', 123, 0, '23, Oak View Avenue', 'Western Australia', 'Melbourne', 'Colombo', 'Disabled', '2018-05-13 01:20:23'),
-(41, 'Mark Taylor', '', 111, 0, '111', '', '', 'Colombo', 'Active', '2018-05-13 01:24:54'),
-(42, 'Nelson Mandela', 'sjobs@apple.com', 333829832, 0, '1st Floor, Apple House, ', 'Las Vegas Street', 'Las Vegas', 'Kalutara', 'Disabled', '2018-05-13 02:39:41');
+INSERT INTO `customer` (`customerID`, `fullName`, `email`, `mobile`, `phone2`, `address`, `address2`, `city`, `district`, `status`, `createdOn`, `password`) VALUES
+(4, 'Bill Gates', 'bill@microsoft.com', '993737', '772484884', '45, Palo Alto House, Marine Drive', 'South Carolina', 'Microsoft', 'Kurunegala', 'Active', '2018-04-30 15:14:02', ''),
+(14, 'Steve Jobs', 'sjobs@apple.com', '333829832', '0', '1st Floor, Apple House, ', 'Las Vegas Street', 'Las Vegas', 'Monaragala', 'Disabled', '2018-05-01 12:03:10', ''),
+(18, 'Asitha Silva', 'asitha@gmail.com', '777987654', '0', 'No. 3, Radcliff Avenue, School Lane', 'Kalutara South', 'Kalutara', 'Kalutara', 'Active', '2018-05-02 09:52:28', ''),
+(24, 'Sunil Perera', 'Sunil@gypsies.sound', '338393932', '413837293', '67/7, Perera Villa, Jayasekara Avenue', 'Mount Lavinia', 'Ratmalana', 'Colombo', 'Active', '2018-05-02 10:48:37', ''),
+(25, 'Theresa May', 'may34@uk.gov.com', '329393903', '777833737', '12, Downing Street', 'London', 'London', 'Matale', 'Active', '2018-05-03 02:28:07', ''),
+(26, 'Sachin Tendulkar', 'sachintendulkar@icc.com', '444958303', '84792838', '789-4, Apartment 3, ', 'Cric Complex', 'New Delhi', 'Puttalam', 'Active', '2018-05-03 02:28:38', ''),
+(38, 'Nuwan Perara', 'nuwan@yahoo.com', '839378202', '0', 'Nuwan Villa, Lower Street,', 'North Mulativu', 'Mullaitivu', 'Mullaitivu', 'Active', '2018-05-05 11:17:49', ''),
+(39, 'Amal Silverton', 'amals452@yahoo.com', '232345676', '0', 'Amal\'s House, Amal\'s Street,', 'Amal Road', 'Ambalangoda', 'Galle', 'Active', '2018-05-05 13:27:06', ''),
+(40, 'Andrew Symonds', 'symonds@cricket.au.com', '123', '0', '23, Oak View Avenue', 'Western Australia', 'Melbourne', 'Colombo', 'Disabled', '2018-05-13 01:20:23', ''),
+(41, 'Mark Taylor', '', '111', '0', '111', '', '', 'Colombo', 'Active', '2018-05-13 01:24:54', ''),
+(42, 'Nelson Mandela', 'sjobs@apple.com', '333829832', '0', '1st Floor, Apple House, ', 'Las Vegas Street', 'Las Vegas', 'Kalutara', 'Disabled', '2018-05-13 02:39:41', ''),
+(44, 'Bill Gates', 'billgates@ms.com', '12345', '56565', 'Malagasang', 'Anabu', 'City', 'Ampara', 'Active', '2023-07-12 04:58:36', ''),
+(49, 'Jerick Arzaga', 'jerick@mail.com', '565656', '0', 'GMA', '', '', 'Ampara', 'Active', '2023-07-13 07:18:41', '$2y$10$wOG1dDv6KbyuwjzOs4/bmO0aYGP9W72c7WIl9EKHFRvGrKvyi9QPa'),
+(50, 'CJ Rosario', 'cj@mail.com', '09951234567', '', 'Molino', '', 'Bacoor', 'Badulla', 'Active', '2023-07-13 07:25:44', '$2y$10$NT7204Br4hssHbmrLTKFne00RfBgwDMc4TmtybIPosFWd3z.fqym6');
 
 -- --------------------------------------------------------
 
@@ -67,21 +72,21 @@ CREATE TABLE `item` (
   `productID` int(11) NOT NULL,
   `itemNumber` varchar(255) NOT NULL,
   `itemName` varchar(255) NOT NULL,
-  `discount` float NOT NULL DEFAULT '0',
-  `stock` int(11) NOT NULL DEFAULT '0',
-  `unitPrice` float NOT NULL DEFAULT '0',
+  `discount` float NOT NULL DEFAULT 0,
+  `stock` int(11) NOT NULL DEFAULT 0,
+  `unitPrice` float NOT NULL DEFAULT 0,
   `imageURL` varchar(255) NOT NULL DEFAULT 'imageNotAvailable.jpg',
   `status` varchar(255) NOT NULL DEFAULT 'Active',
   `description` text NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
 
 --
 -- Dumping data for table `item`
 --
 
 INSERT INTO `item` (`productID`, `itemNumber`, `itemName`, `discount`, `stock`, `unitPrice`, `imageURL`, `status`, `description`) VALUES
-(34, '1', 'First Bag', 0, 28, 1500, '1525670999_1.png', 'Active', ''),
-(35, '2', 'School Bag', 0, 5, 500, '1525681111_661539.png', 'Active', ''),
+(34, '1', 'First Bag', 0, 25, 1500, '1525670999_1.png', 'Active', ''),
+(35, '2', 'School Bag', 1.3, 30, 600, '1525681111_661539.png', 'Active', 'Classic school bag to be used by students.'),
 (36, '3', 'Office Bag', 0, 5, 1300, '1525709924_office bag.jpg', 'Active', ''),
 (37, '4', 'Leather Bag', 2, 6, 3409, '1525710010_leather bag.jpg', 'Active', ''),
 (38, '5', 'Travel Bag', 2, 17, 1200, '1525706032_travel bag.jpg', 'Active', ''),
@@ -90,7 +95,8 @@ INSERT INTO `item` (`productID`, `itemNumber`, `itemName`, `discount`, `stock`, 
 (41, '8', 'Laptop Bag', 2.1, 9, 2300, '1525750683_661539.png', 'Active', ''),
 (43, '10', 'Sports Bag', 1, 92, 1000, '1525756289_sports bag.jpg', 'Active', ''),
 (45, '11', 'First Aid Bag', 1.5, 11, 1200, '1525787551_first aid bag.jpg', 'Active', ''),
-(49, '14', 'Hiking Bag', 1.5, 6, 1200, '1526297640_hiking bag.jpg', 'Active', 'This is a hiking bag. Ideal for long distance hikes. Light-weight and water proof.');
+(49, '14', 'Hiking Bag', 1.5, 6, 1200, '1526297640_hiking bag.jpg', 'Active', 'This is a hiking bag. Ideal for long distance hikes. Light-weight and water proof.'),
+(53, '9', 'Paper Bag', 1.5, 65, 200, '1689134192_OIP.jpg', 'Disabled', 'A durable paper bag');
 
 -- --------------------------------------------------------
 
@@ -103,19 +109,19 @@ CREATE TABLE `purchase` (
   `itemNumber` varchar(255) NOT NULL,
   `purchaseDate` date NOT NULL,
   `itemName` varchar(255) NOT NULL,
-  `unitPrice` float NOT NULL DEFAULT '0',
-  `quantity` int(11) NOT NULL DEFAULT '0',
+  `unitPrice` float NOT NULL DEFAULT 0,
+  `quantity` int(11) NOT NULL DEFAULT 0,
   `vendorName` varchar(255) NOT NULL DEFAULT 'Test Vendor',
-  `vendorID` int(11) NOT NULL DEFAULT '0'
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+  `vendorID` int(11) NOT NULL DEFAULT 0
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
 
 --
 -- Dumping data for table `purchase`
 --
 
 INSERT INTO `purchase` (`purchaseID`, `itemNumber`, `purchaseDate`, `itemName`, `unitPrice`, `quantity`, `vendorName`, `vendorID`) VALUES
-(39, '1', '2018-05-24', 'First Bag', 1600, 10, 'Johnson and Johnsons Co.', 3),
-(40, '2', '2018-05-18', 'First Bag', 2341, 2, 'Louise Vitton Bag', 4),
+(39, '1', '2018-05-24', 'First Bag', 1500, 7, 'Johnson and Johnsons Co.', 3),
+(40, '2', '2018-05-18', 'School Bag', 600, 7, 'Louise Vitton Bag', 4),
 (41, '4', '2018-05-07', 'Leather Bag', 1234, 3, 'Johnson and Johnsons Co.', 3),
 (42, '1', '2018-05-24', 'First Bag', 345, 12, 'Louise Vitton Bag', 4),
 (43, '5', '2018-05-03', 'Travel Bag', 35, 3, 'Johnson and Johnsons Co.', 3),
@@ -126,7 +132,10 @@ INSERT INTO `purchase` (`purchaseID`, `itemNumber`, `purchaseDate`, `itemName`, 
 (48, '1', '2018-05-12', 'Handbag', 2, 9, 'ABC Company', 1),
 (50, '14', '2018-05-15', 'Hiking Bag', 1000, 5, 'Louise Vitton Bag', 4),
 (51, '11', '2018-05-11', 'First Aid Bag', 1121, 1, 'ABC Company', 1),
-(52, '1', '2018-05-21', 'First Bag', 1235, 2, 'Sample Vendor 222', 2);
+(52, '1', '2018-05-21', 'First Bag', 1235, 2, 'Sample Vendor 222', 2),
+(54, '9', '2018-08-24', 'Paper Bag', 200, 10, 'New Bags Exporters', 8),
+(55, '9', '2018-05-24', 'Paper Bag', 200, 10, 'Sky Corp', 6),
+(56, '9', '2018-05-24', 'Paper Bag', 200, 5, 'Sample Vendor 222', 2);
 
 -- --------------------------------------------------------
 
@@ -141,10 +150,10 @@ CREATE TABLE `sale` (
   `customerName` varchar(255) NOT NULL,
   `itemName` varchar(255) NOT NULL,
   `saleDate` date NOT NULL,
-  `discount` float NOT NULL DEFAULT '0',
-  `quantity` int(11) NOT NULL DEFAULT '0',
-  `unitPrice` float(10,0) NOT NULL DEFAULT '0'
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+  `discount` float NOT NULL DEFAULT 0,
+  `quantity` int(11) NOT NULL DEFAULT 0,
+  `unitPrice` float(10,0) NOT NULL DEFAULT 0
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
 
 --
 -- Dumping data for table `sale`
@@ -167,7 +176,9 @@ INSERT INTO `sale` (`saleID`, `itemNumber`, `customerID`, `customerName`, `itemN
 (14, '10', 39, 'Amal Silverton', 'Sports Bag', '2018-05-17', 1, 1, 1000),
 (15, '14', 38, 'Nuwan Perara', 'Hiking Bag', '2018-05-24', 1.5, 1, 1200),
 (16, '1', 14, 'Steve Jobs', 'First Bag', '2018-05-24', 10, 1, 1500),
-(17, '4', 14, 'Steve Jobs', 'Leather Bag', '2018-05-18', 2, 1, 3409);
+(17, '4', 14, 'Steve Jobs', 'Leather Bag', '2018-05-18', 2, 1, 3409),
+(18, '9', 44, 'Bill Gates', 'Paper Bag', '2018-05-24', 1.5, 10, 200),
+(19, '9', 4, 'Bill Gates', 'Paper Bag', '2018-05-24', 1.5, 0, 200);
 
 -- --------------------------------------------------------
 
@@ -181,7 +192,7 @@ CREATE TABLE `user` (
   `username` varchar(255) NOT NULL,
   `password` varchar(255) NOT NULL,
   `status` varchar(255) NOT NULL DEFAULT 'Active'
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
 
 --
 -- Dumping data for table `user`
@@ -207,8 +218,8 @@ CREATE TABLE `vendor` (
   `city` varchar(30) DEFAULT NULL,
   `district` varchar(30) NOT NULL,
   `status` varchar(255) NOT NULL DEFAULT 'Active',
-  `createdOn` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+  `createdOn` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
 
 --
 -- Dumping data for table `vendor`
@@ -219,7 +230,7 @@ INSERT INTO `vendor` (`vendorID`, `fullName`, `email`, `mobile`, `phone2`, `addr
 (2, 'Sample Vendor 222', 'sample@volvo.com', 99828282, 283730183, '123, A Road, B avenue', 'Pitipana', 'Nugegoda', 'Mannar', 'Disabled', '2018-05-05 06:12:02'),
 (3, 'Johnson and Johnsons Co.', '', 32323, 0, '34, Malwatta Road, Kottawa', 'Pannipitiya', 'Maharagama', 'Colombo', 'Active', '2018-05-05 06:28:33'),
 (4, 'Louise Vitton Bag', 'vitton@vitton.usa.com', 323234938, 0, '45, Palmer Valley, 5th Crossing', 'Delaware', 'Palo Alto', 'Batticaloa', 'Active', '2018-05-05 06:29:41'),
-(6, 'Test Vendor', 'test@vendor.com', 43434, 47569937, 'Test address', 'Test address 2', 'Test City', 'Trincomalee', 'Active', '2018-05-05 06:53:14'),
+(6, 'Sky Corp', 'skycorp@vendor.com', 2147483647, 2147483647, 'Paliparan', 'Malagasang,', 'Imus', 'Nuwara Eliya', 'Active', '2018-05-05 06:53:14'),
 (7, 'Bags Co. Exporters Ltd.', '', 1111, 0, 'Sea Road, Bambalapitiya', '', '', 'Colombo', 'Active', '2018-05-15 10:36:54'),
 (8, 'New Bags Exporters', '', 191938930, 0, '123, A Road, B avenue, ', 'Gilford Crescent', 'Colpetty', 'Colombo', 'Active', '2018-05-16 12:36:53');
 
@@ -271,32 +282,39 @@ ALTER TABLE `vendor`
 -- AUTO_INCREMENT for table `customer`
 --
 ALTER TABLE `customer`
-  MODIFY `customerID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=43;
+  MODIFY `customerID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=55;
+
 --
 -- AUTO_INCREMENT for table `item`
 --
 ALTER TABLE `item`
-  MODIFY `productID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=50;
+  MODIFY `productID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=54;
+
 --
 -- AUTO_INCREMENT for table `purchase`
 --
 ALTER TABLE `purchase`
-  MODIFY `purchaseID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=53;
+  MODIFY `purchaseID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=57;
+
 --
 -- AUTO_INCREMENT for table `sale`
 --
 ALTER TABLE `sale`
-  MODIFY `saleID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=18;
+  MODIFY `saleID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=20;
+
 --
 -- AUTO_INCREMENT for table `user`
 --
 ALTER TABLE `user`
   MODIFY `userID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
 --
 -- AUTO_INCREMENT for table `vendor`
 --
 ALTER TABLE `vendor`
   MODIFY `vendorID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+COMMIT;
+
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
